6cd39612c551d576d5180c3b488c25c7c388a75d
nicemonteiro <nicemonteiro@gmail.com>
2026-01-15 19:17:49 -0300

    Primeira versão da calculadora

---
+++ src/estudo.js
@@ -0,0 +1,9 @@
+let userName = getFirsName ("Eunice Nunes Monteiro")
+
+function getFirsName (name){
+    let firstName = name.split (" ") [0 && 2]
+    return firstName
+}
+
+
+console.log("Seja bem-vinda, " + userName)
